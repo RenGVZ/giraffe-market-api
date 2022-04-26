7610ff42a650516373066b85501aca84e3acb479
RenGVZ <gaving@live.com>
2022-04-27 00:59:36 +0900

    added user functions

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 const CryptoJS = require("crypto-js");
 
 // UPDATE
@@ -26,5 +26,44 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     res.status(500).json(err);
   }
 })
+
+//  DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...otherInfo } = user._doc;
+    res.status(200).json(otherInfo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET ALL USERS
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new
+  try {
+    const user = query
+      ? await User.find().sort({ _id: -1 }).limit(2)
+      : await User.find();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET USER STATS
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  
+});
 
 module.exports = router;
