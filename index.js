2ab551c91dde6f7396ba6099367b8a9fe2e974c8
RenGVZ <gaving@live.com>
2022-08-06 13:04:15 +0900

    changes to product model

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const orderRoute = require("./routes/order");
 const orderCart = require("./routes/cart");
+const stripeRoute = require("./routes/stripe")
+const cors = require("cors");
 
 dotenv.config();
 
@@ -17,12 +19,14 @@ mongoose
     console.log(err);
   });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/carts", orderCart);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, ()=> {
   console.log("BE server is running");
